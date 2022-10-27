ba62056b5aa7b1c792182cf45cf0d91b80c704d1
Hezron-tech <kiprophez@gmail.com>
2022-10-27 13:23:00 +0300

    worked on endpoints

--- routes/route.js
+++ routes/route.js
@@ -2,4 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 
+const data={
+    "slackUsername": "Hezron Kiprop",
+    "Backend":true,
+    "age":23,
+    "bio":'Fullstack developer'  
+}
+
+router.get('/getAll' ,(req,res)=>{
+    res.json(data)
+})
+
+
+
+
 module.exports = router;
